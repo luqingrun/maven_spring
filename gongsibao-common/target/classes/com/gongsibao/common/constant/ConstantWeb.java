8dfb2499c81412c8ae08ece811295237074e49a0
luqingrun <luqingrun@gongsibao.com>
2016-04-06 10:25:14 +0800

    del target

--- gongsibao-common/target/classes/com/gongsibao/common/constant/ConstantWeb.java
+++
@@ -1,8 +0,0 @@
-package com.gongsibao.common.constant;
-
-/**
- * Created by luqingrun on 16/3/22.
- */
-public class ConstantWeb {
-    public static final String HTTP_REQUEST_HOST_NAME = "";
-}
--- gongsibao-uc/target/classes/com/gongsibao/uc/controllers/role/RoleController.java
+++
@@ -1,7 +0,0 @@
-package com.gongsibao.uc.controllers.role;
-
-/**
- * Created by luqingrun on 16/3/28.
- */
-public class RoleController {
-}
